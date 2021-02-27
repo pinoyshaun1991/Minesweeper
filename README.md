ec8b1c51c9d5f5ecaeeca02d9336eba107aa0547
Shaun <shaun.brunning@focusimc.co.uk>
2021-02-27 13:40:52 +0000

    Initial Commit.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-FizzBuzz++ Task
+Minesweeper Task
 ===============================
 
 ### Implementation
@@ -7,5 +7,6 @@ This test has been developed using no framework.
 
 ### How to test
 
-1. In the terminal, please type the following to execute the calculate script, `php src/Script/index.php`.
-2. A fizzbuzz.log file should be created within the root of the project displaying the results.
+1. In the terminal, please type the following to execute the script, `php src/Script/index.php`.
+2. You will be presented with a set of questions asking to enter the following; Number of rows, Number of columns & to either enter "mine" or a number between 1-8 (Validation included).
+3. You should then be presented with the structure of the grid.
--- src/Script/index.php
+++ src/Script/index.php
@@ -1,56 +1,34 @@
 <?php
+/** Get input fields **/
+$rows = readline('Please enter number of rows: ');
 
-/** Loops through to 500 and outputs the expected string **/
-$file   = fopen('fizzbuzz.log',"a+") or exit ("Unable to open file!");
-$string = '';
-
-for ($i = 1; $i <= 500; $i++) {
-    switch ($i) {
-        case isPrime($i) === true:
-            $string = $i." FIZZBUZZ++";
-            echo $string."\n";
-            fwrite($file,$string."\n");
-            break;
-        case $i % 15 == 0:
-            $string = $i." FIZZBUZZ";
-            echo $string."\n";
-            fwrite($file,$string."\n");
-            break;
-        case $i % 5 == 0:
-            $string = $i." BUZZ";
-            echo $string."\n";
-            fwrite($file,$string."\n");
-            break;
-        case $i % 3 == 0:
-            $string = $i." FIZZ";
-            echo $string."\n";
-            fwrite($file,$string."\n");
-            break;
-        default:
-            echo $i."\n";
-            fwrite($file,$i."\n");
-    }
+if (is_numeric($rows) == false) {
+    throw new Exception('Number of rows needs to be a number');
 }
 
-fclose($file);
-
-/**
- * Check if given variable is prime or not
- *
- * @param $n
- * @return bool
- */
-function isPrime($n)
-{
-    if ($n == 1) {
-        return false;
-    }
-
-    for ($x=2; $x<$n; $x++) {
-        if ($n %$x ==0) {
-            return false;
-        }
-    }
-
-    return true;
+$columns = readline('Please enter number of columns: ');
+if (is_numeric($columns) == false) {
+    throw new Exception('Number of columns needs to be a number');
 }
+
+$mine = readline('Please enter "mine" for a mine or a number from 1-8: ');
+if ($mine !== 'mine' && is_numeric($mine) == false) {
+    throw new Exception('Input needs to include "mine"');
+}
+
+if (is_numeric($mine) && (int)$mine < 1 || (int)$mine > 8) {
+    throw new Exception('Input needs to be between 1-8');
+}
+
+/** Generate grid **/
+
+for ($i=0; $i<=$columns; $i++) {
+    echo "column ";
+}
+
+for ($i=0; $i<=$rows; $i++) {
+    echo "\n";
+    echo "row\n";
+}
+
+/** Set out logic to determine whether placed location is a mine or how far we are to a mine **/
